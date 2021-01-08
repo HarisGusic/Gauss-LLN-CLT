a2075bc2a4da656ed90e3f38703fa8e3e53ea525
Haris Gušić <harisgusic1@gmail.com>
2021-01-08 01:47:59 +0100

    Miscellaneous changes in code

--- src/shared.py
+++ src/shared.py
@@ -1,37 +1,13 @@
 import numpy as np; from numpy import *
 
 def pdf_mean(f, p):
-    return (f * p).sum()
+    return sum(f * p)
 def pdf_mean_square(x, p):
     return pdf_mean(x**2, p)
 def pdf_var(x, p):
     return pdf_mean_square(x,p) - pdf_mean(x,p)**2
-
-def gaussian(x, mean=0.0, var=1.0):
-    return sqrt(exp(-(x-mean)**2 / var) / (2*pi*var))
-
-def multivariate_gaussian(x, mean, cov):
-    x = array(x); mean = array(mean); cov = array(cov)
-    k = x.shape[0] # Broj varijabli
-    n = x.shape[1] # Broj ulaznih koordinata
-    
-    # Provjera ulaznih parametara
-    #if len(x.shape) != 1:
-        #raise Exception('Neispravan vektor ulaznih promjenljivih x');
-    #if shape(cov)[0] != shape(cov)[1] or any(cov != cov.T) or any(linalg.eig(cov) < 0):
-        #raise Exception('Neispravna kovarijantna matrica')
-    #TODO provjere
-    
-    inv_cov = matrix(linalg.inv(cov))
-    inv_det = linalg.det(inv_cov)
-    
-    y = empty(n)
-    for j in range(n):
-        x_m = matrix(x)[:,j] - matrix(mean).T
-        y[j] = sqrt(exp(- x_m.T * inv_cov * x_m).T * inv_det / (2*pi)**k)
-    
-    return y
-
+def integral(x, f):
+    return sum((f + roll(f, -1))[:-1] * diff(x)) / 2
 
 def cov_elipsa(mean, cov, N=50):
     a = cov[0][0]; b = cov[0][1]; c = cov[1][1]
