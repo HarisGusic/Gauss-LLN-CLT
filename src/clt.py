fa8abb47af1af4cecd1f3a3f8515781f3ac19105
Haris Gušić <harisgusic1@gmail.com>
2021-01-10 18:17:00 +0100

    Clean up code, mainly in CLT

--- src/clt.py
+++ src/clt.py
@@ -1,6 +1,8 @@
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import numpy as np; from numpy import *
+from shared import *
+from gauss import *
 
 def plot_binomial(n, approx='Auto'):
     k = range(n+1)
@@ -18,3 +20,29 @@ def plot_binomial(n, approx='Auto'):
     # Anotacija
     plt.xlabel('$k$')
     plt.legend(loc='upper right', bbox_to_anchor=(0.99,0.92), framealpha=1)
+
+def plot_fun(f):
+    plt.plot(arange(-50, 50), f)
+    plt.yticks([], [])
+    plt.tight_layout()
+    plt.xlabel('$x$'); plt.ylabel('$f(x)$')
+    
+def plot_convolution(f, n):
+    # Formiranje normalizovane konvolucije
+    g = convolve_n(f, n)
+    g = g / sum(g)
+    
+    # Formiranje ulazne varijable
+    N = len(g)
+    x = array(range(N)) - N/2
+    
+    # Aproksimacija Gauss-ovom funkcijom
+    g_approx = approx_gaussian(x, g)
+    
+    # Plot
+    plt.plot(x, g, label='Konvolucija');
+    plt.plot(x, g_approx, label='Aproksimacija')
+    # Anotacija i estetika
+    plt.yticks([], [])
+    plt.xlabel('$x$'); plt.ylabel('$g(x)$'); plt.legend()
+    
