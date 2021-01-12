fe553818afae3b82c8b370d8c8dc815936691ae6
Haris Gušić <harisgusic1@gmail.com>
2021-01-12 09:22:34 +0100

    Progress save

--- scripts/common_plot.py
+++ scripts/common_plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
-rc('font', size=18)
+rc('font', size=20)
 
 def savefig(path):
     plt.savefig('_build/img/' + path + '.pdf', format='pdf', pad_inches=0,
