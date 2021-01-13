cd8ef2e46751222c699b7883834cee998465d0f6
Haris Gušić <harisgusic1@gmail.com>
2021-01-13 03:29:51 +0100

    Progress save

--- src/lln.py
+++ src/lln.py
@@ -12,6 +12,16 @@ def gen_rnd_sequence(N_sekv, N_realiz, tip, mean=0, std=1):
         return rnd.exponential(std, (N_realiz, N_sekv))
     elif tip == 'normal':
         return rnd.normal(mean, std, (N_realiz, N_sekv))
+    elif tip == 'normal_converging_var':
+        X_n = empty((N_realiz, N_sekv))
+        for j in range(N_sekv):
+            X_n[:, j] = rnd.normal(mean, std * (1 - exp(-(j+1))), N_realiz)
+        return X_n if N_realiz == 1 else X_n[0]
+    elif tip == 'normal_diverging_var':
+        X_n = empty((N_realiz, N_sekv))
+        for j in range(N_sekv):
+            X_n[:, j] = rnd.normal(mean, std * (j+1), N_realiz)
+        return X_n if N_realiz == 1 else X_n[0]
 
 def gen_pdf(x, tip, mean, std):
     if tip == 'uniform':
@@ -19,7 +29,21 @@ def gen_pdf(x, tip, mean, std):
     elif tip == 'exponential':
         return stats.expon.pdf(x, 0, std)
     elif tip == 'normal':
-        return stats.exp.normal(x, mean, std)
+        return stats.norm.pdf(x, mean, std)
+
+def histogram(X, bins=10):
+    # Određivanje raspona vrijednosti
+    a = min(X); b = max(X)
+    # Širina intervala za svaku "korpu"
+    dx = (b - a) / bins
+    hist = zeros(bins)
+    for i in range(len(X)):
+        k = int((X[i]-a)/dx)
+        if k < len(hist):
+            hist[k] += 1
+        else: # Uslijed numeričke nepreciznosti može biti van opsega
+            hist[k-1] += 1
+    return hist, linspace(a, b, bins), dx
 
 # N_sekv - veličina sekvence
 # N_realiz - broj realizacija koje se prikazuju
@@ -36,4 +60,22 @@ def plot_lln(X_n, mean, plot_range=[-2, 2], colored=True):
     plt.plot(n_multi, S_n.T, c=None if colored else 'k')
     plt.plot([n[0], n[-1]], [mean, mean],
              label='Očekivanje $\mu$', c='k' if colored else 'red')
-    plt.legend(); plt.title('Realizacije sekvence $\{S_n\}$')
+    plt.legend(); plt.xlabel('$n$')
+
+def plot_hist(tip, N, bins=10):
+    
+    # Generisanje slučajne varijable i histograma
+    X = gen_rnd_sequence(N, 1, tip, 0, 1)[0]
+    hist, x, dx = histogram(X, bins)
+    
+    # Prikaz histograma
+    bars = plt.bar(x, hist / (N*dx), width=dx,
+                   align='edge', color='wheat', label='Histogram')
+    for bar in bars:
+        bar.set_edgecolor('sandybrown')
+        bar.set_linewidth(2.6)
+
+    # Prikaz PDF
+    x = arange(-4, 4, 0.1)
+    plt.plot(x, gen_pdf(x, tip, 0, 1), c='k', label='$p_X(x)$')
+    plt.legend(); plt.xlabel('$x$'); plt.yticks([], [])
