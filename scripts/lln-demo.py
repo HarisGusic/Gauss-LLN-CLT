fe16a37f4b0d0d24ea44d5c04bfada908cf89192
Haris Gušić <harisgusic.dev@gmail.com>
2021-01-13 03:29:51 +0100

    Progress save

--- scripts/lln-demo.py
+++ scripts/lln-demo.py
@@ -4,7 +4,7 @@ from lln import *
 import os
 exec(open(os.path.dirname(__file__) + '/common_plot.py').read())
 
-rnd.seed(1) # Always generate the same plot
+rnd.seed(10) # Always generate the same plot
 
 X_n = gen_rnd_sequence(100, 3, 'normal', 0, 1)
 plt.figure()
@@ -20,3 +20,8 @@ X_n = gen_rnd_sequence(100, 50, 'normal', 0, 2)
 plt.figure()
 plot_lln(X_n, 0)
 savefig('lln_demo_3')
+
+X_n = gen_rnd_sequence(100, 5, 'normal_diverging_var', 0, 0.1)
+plt.figure()
+plot_lln(X_n, 0)
+savefig('lln_demo_4')
